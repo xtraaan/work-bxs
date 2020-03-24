e7721e5d25d2763436ac807b035ea18d846df0e6
Eilynn Tran <eilynntran@gmail.com>
2020-03-24 13:15:45 -0500

    Update script.py

--- covid_19_dashboard/script.py
+++ covid_19_dashboard/script.py
@@ -31,25 +31,28 @@ def get_weekly_stats():
     data = pd.read_csv('../../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
 
     # First 170 rows in file, to eliminate US counties for now
-    data = data.head(170)
+    data = data.head(200)
 
-    # Reading the headers
-    # print(data.columns)
-
-    # Get weekly data from all 52 states
+    # Get only United States data
     weekly_data = data.loc[data['Country/Region'] == "US"]
 
-    # Gives syntax error??
-    # weekly_data = weekly_data.iloc[[0:2] + [-7:]]
+    # Gives syntax error? Not sure why.
+    # weekly_data = weekly_data.iloc[:, [0:2] + [-7:]]
 
-    weekly_data = weekly_data.iloc[:, [0, 1, -7, -6, -5, -4, -3, -2, -1]]
+    weekly_data = data.iloc[:, [0, 1, -7, -6, -5, -4, -3, -2, -1]]
 
     # Sort data from most confirm cases to least
-    weekly_data = weekly_data.sort_values(data.columns[-1], ascending = False)
+    weekly_data = weekly_data.sort_values('Province/State')
 
-    # Save weekly modified CSV and JSON file
+    # Save weekly modified CSV
     weekly_data.to_csv('covid-19_weekly-modified-csv.csv')
-    weekly_data.to_json('covid-19_weekly-modified-json.json')
+
+    # Reads new modified CSV file and removes Unnamed column to fix key values
+    # Then saves new CSV and JSON file
+    weekly_data = pd.read_csv('covid-19_weekly-modified-csv.csv')
+    weekly_data = weekly_data.loc[:, ~weekly_data.columns.str.contains('^Unnamed')]
+    weekly_data.to_csv('covid-19_weekly-modified-csv.csv')
+    weekly_data.to_json('covid-19_weekly-modified-json.json', orient='table')
 
 
 # Run functions: 1) Update COVID-19 repository 2) Update modified CSV with recent changes
